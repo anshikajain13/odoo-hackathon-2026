dc0ab7d6eeb7a41a8a0dd17a0bbdb4f44fc58ee1
Hardik Gupta <hardikgupta2895@gmail.com>
2026-07-12 12:36:45 +0530

    Build vehicle CRUD API

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 import models
 
 from database import engine
+from routers import vehicles
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,6 +14,9 @@ app = FastAPI(
     description="Smart Transport Operations Platform",
     version="1.0"
 )
+
+
+app.include_router(vehicles.router)
 
 
 @app.get("/")
